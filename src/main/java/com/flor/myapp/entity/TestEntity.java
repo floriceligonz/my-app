616a188a7760b20684deb3b6363292008c76c0f5
floriceligonz <floriruiz77@gmail.com>
2024-02-19 19:59:13 -0600

    Nombre: my-app

---
+++ src/main/java/com/flor/myapp/entity/TestEntity.java
@@ -0,0 +1,21 @@
+package com.flor.myapp.entity;
+
+public class TestEntity {
+
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
